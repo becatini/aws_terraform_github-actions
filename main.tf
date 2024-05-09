d45d4bc5929b24bd752a4183c90cfc54a3020ee7
becatini <19430469+becatini@users.noreply.github.com>
2024-05-09 15:39:43 -0600

    .

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "aws" {
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
 
   name = "vpc-eks"
   cidr = "10.0.0.0/16"
@@ -27,5 +28,30 @@ module "vpc" {
   tags = {
     Terraform = "true"
     Environment = "dev"
+    Project = "eks"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.10.0"
+
+  cluster_name = "eks-dev"
+  cluster_version = "1.29"
+
+  enable_cluster_creator_admin_permissions = true
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id = module.vpc.vpc_id
+
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    default {
+        min_size = 2
+        max_size = 2
+        desired_size = 2
+        instance_types=["t3.small"]
+    }
   }
 }
