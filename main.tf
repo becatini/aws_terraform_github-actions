8f8bbfa3c289088a79e451ad5dd89abd17ef6de3
becatini <19430469+becatini@users.noreply.github.com>
2024-05-09 10:23:09 -0600

    .

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "vpc-eks"
-  cidr = "10.0.1.0/16"
+  cidr = "10.0.0.0/16"
 
   azs             = ["us-west-1a", "us-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
