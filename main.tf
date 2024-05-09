e6b99c6c5ae7a031287c94d6141722ee747ef3c4
becatini <19430469+becatini@users.noreply.github.com>
2024-05-09 10:20:15 -0600

    .

--- main.tf
+++ main.tf
@@ -8,24 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  region = us-west-1
+  region = "us-west-1"
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "vpc-eks"
-  cidr = "10.0.1.0/16"
-
-  azs             = ["us-west-1a", "us-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
