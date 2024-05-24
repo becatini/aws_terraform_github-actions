ecf58c6f5f494d22564c888b65117f87530744b8
becatini <19430469+becatini@users.noreply.github.com>
2024-05-24 10:36:20 -0600

    Code updated

--- src/main.tf
+++ src/main.tf
@@ -1,57 +1,57 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "5.8.1"
 
-  name = var.aws_vpc_name
-  cidr = var.aws_vpc_cidr
+#   name = var.aws_vpc_name
+#   cidr = var.aws_vpc_cidr
 
-  azs             = var.aws_vpc_azs
-  private_subnets = var.aws_private_subnets
-  public_subnets  = var.aws_public_subnets
+#   azs             = var.aws_vpc_azs
+#   private_subnets = var.aws_private_subnets
+#   public_subnets  = var.aws_public_subnets
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = false
+#   enable_nat_gateway = true
+#   enable_vpn_gateway = false
 
-  # The "merge" will use the tags "tags = var.aws_project_tags" to work with the EKS tags
-  # tags = var.aws_project_tags
-  tags = merge(var.aws_project_tags, { "kubernetes.io/cluster/${var.aws_eks_name}" = "shared" })
+#   # The "merge" will use the tags "tags = var.aws_project_tags" to work with the EKS tags
+#   # tags = var.aws_project_tags
+#   tags = merge(var.aws_project_tags, { "kubernetes.io/cluster/${var.aws_eks_name}" = "shared" })
 
-  # Tags for EKS to use the VPC
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${var.aws_eks_name}" = "shared"
-    "kubernetes.io/role/elb"                    = 1
-  }
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${var.aws_eks_name}" = "shared"
-    "kubernetes.io/role/internal-elb"           = 1
-  }
+#   # Tags for EKS to use the VPC
+#   public_subnet_tags = {
+#     "kubernetes.io/cluster/${var.aws_eks_name}" = "shared"
+#     "kubernetes.io/role/elb"                    = 1
+#   }
+#   private_subnet_tags = {
+#     "kubernetes.io/cluster/${var.aws_eks_name}" = "shared"
+#     "kubernetes.io/role/internal-elb"           = 1
+#   }
 
-}
+# }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.10.0"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "20.10.0"
 
-  cluster_name    = var.aws_eks_name
-  cluster_version = var.aws_eks_version
+#   cluster_name    = var.aws_eks_name
+#   cluster_version = var.aws_eks_version
 
-  enable_cluster_creator_admin_permissions = true
+#   enable_cluster_creator_admin_permissions = true
 
-  subnet_ids = module.vpc.private_subnets
-  vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+#   vpc_id     = module.vpc.vpc_id
 
-  cluster_endpoint_public_access = true
+#   cluster_endpoint_public_access = true
 
-  eks_managed_node_groups = {
-    default = {
-      min_size       = 2
-      max_size       = 2
-      desired_size   = 2
-      instance_types = var.aws_eks_managed_node_groups_instance_types
-      tags           = var.aws_project_tags
-    }
-  }
+#   eks_managed_node_groups = {
+#     default = {
+#       min_size       = 2
+#       max_size       = 2
+#       desired_size   = 2
+#       instance_types = var.aws_eks_managed_node_groups_instance_types
+#       tags           = var.aws_project_tags
+#     }
+#   }
 
-  tags = var.aws_project_tags
-}
+#   tags = var.aws_project_tags
+# }
 
